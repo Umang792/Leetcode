f4edbb3c0c048f1d0278b11a5b24457734afed55
Umang792 <rajumang47@gmail.com>
2025-12-20 22:52:48 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (45.16%) - LeetSync

---
+++ 263-ugly-number/ugly-number.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    bool isUgly(int n) {
+     if(n<0){
+        return false;
+     }
+     if(n==0){
+        return false;
+     }
+
+
+       while(n){
+        if(n%2==0){
+            n=n/2;
+        }
+        else{
+            break;
+        }
+       }
+
+       while(n){
+        if(n%3==0){
+            n=n/3;
+        }
+        else{
+            break;
+        }
+       }
+
+       while(n){
+        if(n%5==0){
+            n=n/5;
+        }
+        else{
+            break;
+        }
+       }
+      if(n<=1){
+        return true;
+      }
+
+     return false;
+
+
+    }
+};
