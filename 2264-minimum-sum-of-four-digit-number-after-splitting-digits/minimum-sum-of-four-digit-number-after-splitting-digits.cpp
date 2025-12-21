6e77d36a709713844a4345236c0aa63d4f1e41ae
Umang792 <rajumang47@gmail.com>
2025-12-21 15:32:21 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (22.02%) - LeetSync

---
+++ 2264-minimum-sum-of-four-digit-number-after-splitting-digits/minimum-sum-of-four-digit-number-after-splitting-digits.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int minimumSum(int num) {
+     vector<int>v;
+     while(num){
+        int r=num%10;
+        v.push_back(r);
+        num=num/10;
+     }
+     sort(v.begin(),v.end());
+
+     int num1 = v[0] * 10 + v[2];
+     int num2 = v[1] * 10 + v[3];
+     return num1+num2;
+
+    }
+};
