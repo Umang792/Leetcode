e35ff037b6d67a6a2788019205e329d432dd4d87
Umang792 <rajumang47@gmail.com>
2026-01-09 23:57:58 +0530

    Time: 2 ms (61.64%) | Memory: 11.8 MB (47.49%) - LeetSync

--- 383-ransom-note/ransom-note.cpp
+++ 383-ransom-note/ransom-note.cpp
@@ -1,54 +1,54 @@
-class Solution {
-public:
-    bool canConstruct(string ransomNote, string magazine) {
-        map<char,int>magi;
-        map<char,int>ranu;
-
-        for(int i=0;i<magazine.size();i++){
-            magi[magazine[i]]++;
-        }
-
-        for(int i=0;i<ransomNote.size();i++){
-            ranu[ransomNote[i]]++;
-        }
-
-
-        for(auto it:ranu){
-            char c=it.first;
-            int freq=it.second;
-            int freq1=magi[c];
-            if(freq>freq1){
-                return false;
-            }
-        }
-        return true;
-    }
-};
-
-
-
 // class Solution {
 // public:
 //     bool canConstruct(string ransomNote, string magazine) {
-//         int freq[26] = {0};
+//         map<char,int>magi;
+//         map<char,int>ranu;
 
-//         // Store count of each character from magazine
-//         for (int i = 0; i < magazine.length(); i++) {
-//             freq[magazine[i] - 'a']++;
+//         for(int i=0;i<magazine.size();i++){
+//             magi[magazine[i]]++;
 //         }
 
-//         // Check ransomNote characters
-//         for (int i = 0; i < ransomNote.length(); i++) {
-//             int idx = ransomNote[i] - 'a';
+//         for(int i=0;i<ransomNote.size();i++){
+//             ranu[ransomNote[i]]++;
+//         }
 
-//             if (freq[idx] == 0) {
-//                 return false;   // character not available
+
+//         for(auto it:ranu){
+//             char c=it.first;
+//             int freq=it.second;
+//             int freq1=magi[c];
+//             if(freq>freq1){
+//                 return false;
 //             }
-
-//             freq[idx]--;       // use character
 //         }
-
 //         return true;
 //     }
 // };
+
+
+
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        int freq[26] = {0};
+
+        // Store count of each character from magazine
+        for (int i = 0; i < magazine.length(); i++) {
+            freq[magazine[i] - 'a']++;
+        }
+
+        // Check ransomNote characters
+        for (int i = 0; i < ransomNote.length(); i++) {
+            int idx = ransomNote[i] - 'a';
+
+            if (freq[idx] == 0) {
+                return false;   // character not available
+            }
+
+            freq[idx]--;       // use character
+        }
+
+        return true;
+    }
+};
 
