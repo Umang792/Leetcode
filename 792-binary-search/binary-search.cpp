c4fe4521e9a0453a3d98a52b9d465f2f030756a2
Umang792 <rajumang47@gmail.com>
2026-01-13 21:20:10 +0530

    Time: 0 ms (100.00%) | Memory: 31.3 MB (44.20%) - LeetSync

---
+++ 792-binary-search/binary-search.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int n=nums.size();
+        int start=0;
+        int end=n-1;
+        int ans=-1;
+
+
+        while(start<=end){
+            int mid=start+(end-start)/2;
+
+            if(nums[mid]==target){
+                ans=mid;
+                break;
+                
+            }
+            else if(nums[mid]<target){
+                start=mid+1;
+
+            }
+            else{
+                end=mid-1;
+            }
+        }
+
+
+
+        return ans;
+    }
+};
