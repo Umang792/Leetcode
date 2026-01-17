ac9b8d77ee93070ee619e9116414fe4d555810c0
Umang792 <rajumang47@gmail.com>
2026-01-18 00:45:33 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (69.53%) - LeetSync

---
+++ 319-bulb-switcher/bulb-switcher.cpp
@@ -0,0 +1,11 @@
+//if i want to find total number of perfect square in between 1 to n is 
+//square root of n
+//ex n=100 so squaret of 100 =10; so ans is 10
+
+class Solution {
+public:
+    int bulbSwitch(int n) {
+        int ans=sqrt(n);
+        return ans;
+    }
+};
