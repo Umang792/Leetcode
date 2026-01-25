4a27a0fabf371e0708a7f3bb06c3d0680af5556b
Umang792 <rajumang47@gmail.com>
2026-01-25 06:55:45 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (61.26%) - LeetSync

--- 78-subsets/subsets.cpp
+++ 78-subsets/subsets.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
   void helper(vector<int>& num,int index,vector<vector<int>>&ans,vector<int>& temp){
-       
+       if(index==num.size()){
         ans.push_back(temp);
-        
-        for(int i=index;i<num.size();i++){
-            temp.push_back(num[i]);
-            helper(num,i+1,ans,temp);
-            temp.pop_back();
+        return;
         }
+            temp.push_back(num[index]);
+            helper(num,index+1,ans,temp);
+            temp.pop_back();
+            helper(num,index+1,ans,temp);
 
     }
     vector<vector<int>> subsets(vector<int>& nums) {
@@ -20,3 +20,28 @@ public:
       return ans;  
     }
 };
+
+
+
+// class Solution {
+// public:
+//   void helper(vector<int>& num,int index,vector<vector<int>>&ans,vector<int>& temp){
+       
+//         ans.push_back(temp);
+        
+//         for(int i=index;i<num.size();i++){
+//             temp.push_back(num[i]);
+//             helper(num,i+1,ans,temp);
+//             temp.pop_back();
+//         }
+
+//     }
+//     vector<vector<int>> subsets(vector<int>& nums) {
+//       vector<vector<int>>ans;
+//       vector<int>temp;
+//       int index=0;
+//       helper(nums,index,ans,temp);
+
+//       return ans;  
+//     }
+// };
