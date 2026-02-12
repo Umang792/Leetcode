8a56eb14e0dd5c250a6846fc7d391fd543f5e0b4
Umang792 <rajumang47@gmail.com>
2026-02-12 21:24:37 +0530

    Time: 17 ms (24.00%) | Memory: 15.6 MB (68.86%) - LeetSync

---
+++ 775-n-ary-tree-preorder-traversal/n-ary-tree-preorder-traversal.cpp
@@ -0,0 +1,40 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+public:
+    void helper(Node*root,vector<int>&ans){
+        if(root==NULL)return;
+
+        ans.push_back(root->val);//node
+        
+        for(Node*umg:root->children){
+            helper(umg,ans);
+        }
+
+
+    }
+    vector<int> preorder(Node* root) {
+       vector<int>ans;
+       if(root==NULL)return ans;
+       helper(root,ans);
+       return ans; 
+    }
+};
